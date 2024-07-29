206815146f9906b107e8f429437f4a3604416832
Boozmanras <paulvictoraa@gmail.com>
2024-07-29 12:23:29 -0700

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,20 +12,26 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
 
     # Fetch user data
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_url = (
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    )
     user_data = requests.get(user_url).json()
     username = user_data.get('username')
 
     # Fetch todos data
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
     todos_data = requests.get(todos_url).json()
 
     # Prepare data for JSON export
-    tasks = [{
-        "task": todo.get("title"),
-        "completed": todo.get("completed"),
-        "username": username
-    } for todo in todos_data]
+    tasks = [
+        {
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": username
+        } for todo in todos_data
+    ]
 
     data = {employee_id: tasks}
 
