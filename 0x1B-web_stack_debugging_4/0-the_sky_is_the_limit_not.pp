fbfc5aa6a9d8acc4efa7ddb5987c2e75b8fe1b2c
Boozmanras <paulvictoraa@gmail.com>
2024-08-23 00:53:32 +0300

    Skis the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,7 @@
 #Configuring NGiX to accept more traffic
 
-exec('configure maximum open files allowance': command ='sed-i"s/15/4096"/etc/default/nginx/restart',
-path => 'usr/local/sbin:/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-} 
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+
+}
