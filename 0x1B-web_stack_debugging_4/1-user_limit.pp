ea8182fa39d2d8398be7135e76ee6caf59cb8487
Boozmanras <paulvictoraa@gmail.com>
2024-08-23 23:01:02 +0300

    dis

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,11 @@
-iAdjust the operating system settings so that the 'holberton' user can log in and access files without any errors
+# This Puppet manifest adjusts the file limit settings for the 'holberton' user to ensure proper login and file access without issues.
 
-
-exec { 'Enhance the file limit settings for the holberton user':
+exec { 'Enhance the hard file limit settings for holberton user':
   command => "sed -i '/^holberton.*hard.*nofile/s/[0-9]\+/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin:/bin:/usr/bin',
 }
 
-exec { 'Raise the soft file limit for the holberton user':
+exec { 'Raise the soft file limit for holberton user':
   command => "sed -i '/^holberton.*soft.*nofile/s/[0-9]\+/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin:/bin:/usr/bin',
 }
