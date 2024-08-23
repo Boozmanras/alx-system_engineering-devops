11c204baebb602bfab3b97461b7d077a15bbb40a
Boozmanras <paulvictoraa@gmail.com>
2024-08-23 22:58:31 +0300

    dis

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,12 @@
-#Adjust the operating system settings so that the 'holberton' user can log in and access files without any errors
+iAdjust the operating system settings so that the 'holberton' user can log in and access files without any errors
 
-exec { 'Enhance the file limit settings for the 'holberton' user':
 
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-
-  path    => '/usr/local/bin/:/bin/'
-
+exec { 'Enhance the file limit settings for the holberton user':
+  command => "sed -i '/^holberton.*hard.*nofile/s/[0-9]\+/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
 }
 
-exec { 'Raise the soft file limit for the 'holberton' user':
-
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-
-  path    => '/usr/local/bin/:/bin/'
-
+exec { 'Raise the soft file limit for the holberton user':
+  command => "sed -i '/^holberton.*soft.*nofile/s/[0-9]\+/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
 }
