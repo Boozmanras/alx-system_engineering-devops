1cd7af82b77dc368a0fb3b2c051012c078147240
Boozmanras <paulvictoraa@gmail.com>
2024-08-24 11:12:29 +0300

    new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+This module defines a function to query the Reddit API.
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+    Queries the Reddit API to retrieve the total number of subscribers
+    for a given subreddit.
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if
+             the subreddit is invalid or the request fails.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    response = get(url, headers=user_agent)
+
+    try:
+        all_data = response.json()
+        return all_data.get('data', {}).get('subscribers', 0)
+    except Exception:
+        return 0
