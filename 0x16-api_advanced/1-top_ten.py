b49022a5a59f500751107b0ba94637936e2f792a
Boozmanras <paulvictoraa@gmail.com>
2024-08-24 11:19:41 +0300

    canges

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,44 @@
 #!/usr/bin/python3
-import requests
+"""
+This module defines a function to query the Reddit API and print the titles
+of the top 10 hot posts in a given subreddit.
+"""
+
+from requests import get
+
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        posts = response.json().get('data', {}).get('children', [])
+    """
+    Queries the Reddit API to retrieve and print the titles of the first
+    10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None: If the subreddit is invalid or if
+        the request fails, it prints 'None'.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    # Set a custom User-Agent to avoid request blocking by Reddit
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+
+    # Perform the GET request to Reddit API
+    response = get(url, headers=user_agent, params=params)
+
+    try:
+        # Parse the JSON response to retrieve post titles
+        all_data = response.json()
+        posts = all_data.get('data', {}).get('children', [])
+
         for post in posts:
-            print(post.get('data', {}).get('title', None))
-    else:
-        print(None)
+            title = post.get('data', {}).get('title', "None")
+            print(title)
 
-
+    except Exception:
+        print("None")
