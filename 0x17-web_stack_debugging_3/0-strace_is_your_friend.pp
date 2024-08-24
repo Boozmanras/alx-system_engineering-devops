4a6f7b2cdf590236c3a7468740b51c3ff3e83b78
Boozmanras <paulvictoraa@gmail.com>
2024-08-24 09:30:22 +0300

    upd

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,14 @@
-# This manifest fixes the Apache 500 error by ensuring the correct PHP module is enabled
+# This manifest fixes the Apache 500 error by ensuring the correct PHP module is enabled.
+
 exec { 'fix-apache':
   command => '/usr/sbin/a2enmod php5',
   path    => ['/usr/bin', '/usr/sbin'],
   onlyif  => 'test ! -f /etc/apache2/mods-enabled/php5.load',
+  notify  => Exec['restart-apache'],
+}
+
+exec { 'restart-apache':
+  command => '/usr/sbin/service apache2 restart',
+  path    => ['/usr/bin', '/usr/sbin'],
+  refreshonly => true,
 }
