53c3665eb4a1435d925c51c93dcca700157cfddf
Boozmanras <paulvictoraa@gmail.com>
2024-08-24 09:54:57 +0300

    new node

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""Module for querying the number of subscribers for a given subreddit."""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
-    return 0
 
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API to retrieve the number of subscribers for a given subreddit. Args:
+        subreddit (str): The name of the subreddit to query
+    Returns:
+        int: The number of subscribers if the subreddit exists, otherwise 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code >= 300:
+        return 0
+
+    return response.json().get("data", {}).get("subscribers", 0)
